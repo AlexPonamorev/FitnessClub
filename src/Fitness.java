37e15836d983341ad833fce2d01ae67087a7f7e3
AlexPonamorev <ponamorev.alex2012@gmail.com>
2020-10-18 05:29:54 +0300

    current

--- src/Fitness.java
+++ src/Fitness.java
@@ -5,23 +5,12 @@ public class Fitness {
     // где установит время?
     boolean Status;
 
-    private final int MAX = 20;
-    private Subscription[] pooL;
-    private Subscription[] gyM;
-    private Subscription[] grouP;
-
     private ZoneType zoneType;
     private ZoneType zoneType2;
     SubscriptionType subscriptionType;
-    Zones zones;
-    public  Fitness(){
-        zones = new Zones();
-        this.pooL = new Subscription[MAX];
-        this.gyM = new Subscription[MAX];
-        this.grouP = new Subscription[MAX];
-    }
-
+    private Client client;
     private Reception reception;
+
     // здесь я создаю ресепшн
     // иначе никак, так как внешний интерфейс программ не знает ничего про класс Reception
     // и туда отправляем клиента
@@ -32,39 +21,6 @@ public class Fitness {
 
     }
 
-
-
-    //int[ ] a = new int [100];
-    // Здесь у фитнеса можно запросить желаемую зону в зависимости от выбранной клиентом
-    public Subscription[] getZone(ZoneType zoneType) {
-        switch (zoneType) {
-            case POOL:
-                return  pooL = new Subscription [20];
-            case GYM:
-                return  gyM = new Subscription[20];
-            case GROUP:
-                return  grouP = new Subscription[20];
-            default:
-                throw new IllegalArgumentException(Information.INVALID_INPUT);
-        }
-    }
-
-
-
-
-
-
-
-
-
-    public void SetReception() {
-        reception = new Reception();
-    }
-
-    //private Subscription subscription;
-    // создаю ссылку на клиента когад клиент приходит в фитнесс - инициализирую ее клиентом в сеттере
-    private Client client;
-
     public void SetClientANDZone(Client client) {
         this.client = new Client();
 
@@ -74,56 +30,5 @@ public class Fitness {
     public void SetSubscribtion() {
         reception.subscribe(this.client, this.zoneType, this.subscriptionType);
     }
-
-    public void subscribe(Subscription subscription,ZoneType zoneType2){
-        if (checkVisitTime(subscription)){
-            return;
-        }
-
-        switch (zoneType2){
-            case GYM:
-
-                addInArr(gyM,subscription);
-                subscription.setCurrentDate();
-                break;
-            case POOL:
-
-                addInArr(pooL,subscription);
-                subscription.setCurrentDate();
-                break;
-            case GROUP:
-
-                addInArr(grouP,subscription);
-                subscription.setCurrentDate();
-                break;
-            default:
-                throw new IllegalArgumentException(Information.INVALID_INPUT);
-        }
-    }
-
-
-    private void addInArr(Subscription[] subscriptions, Subscription abonement){
-        for (int i = 0; i < subscriptions.length; i++) {
-            if (Objects.isNull(subscriptions[i])){
-                subscriptions[i] = abonement;
-                return;
-            }
-        }
-        System.out.println(Information.IS_CROWDED);
-    }
-
-
-    private boolean checkVisitTime(Subscription subscription){
-        if (subscription.getCurrentDate() < subscription.getStartTime()){
-            System.out.println("Вам придется немного подождать");
-            return true;
-        }else if (subscription.getCurrentDate() > subscription.getEndTime()){
-            System.out.println("Вы опоздали, приходите завтра");
-            return true;
-        }
-        return false;
-    }
-
-
 
 }
--- src/Zones.java
+++ src/Zones.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Zones {
 
     int countClient = 0;
@@ -10,19 +12,63 @@ public class Zones {
    ZoneType zoneType;
    // зона спрашивает у фитнеса куда сейчас записывать
 
-
+    private final int MAX = 20;
     private Subscription[] pooL;
     private Subscription[] gyM;
     private Subscription[] grouP;
 
-    public void AddZone(Subscription subscription, ZoneType zoneType) {
-        if (subscription.getZoneType() == zoneType) {
-            for (int i = 0; i < subscriptions.length; ++i) {
-                if (subscriptions[i] == null) {
-                    subscriptions[i] = subscription;
-                }
+    Zones zones;
+    public  Zones(){
+        zones = new Zones();
+        this.pooL = new Subscription[MAX];
+        this.gyM = new Subscription[MAX];
+        this.grouP = new Subscription[MAX];
+    }
+
+
+    public void subscribe(Subscription subscription,ZoneType zoneType2){
+        if (checkVisitTime(subscription)){
+            return;
+        }
+
+        switch (zoneType2){
+            case GYM:
+                addInArr(gyM,subscription);
+                subscription.setCurrentDate();
+                break;
+            case POOL:
+                addInArr(pooL,subscription);
+                subscription.setCurrentDate();
+                break;
+            case GROUP:
+                addInArr(grouP,subscription);
+                subscription.setCurrentDate();
+                break;
+            default:
+                throw new IllegalArgumentException(Information.INVALID_INPUT);
+        }
+    }
+
+    private void addInArr(Subscription[] subscriptions, Subscription abonement){
+        for (int i = 0; i < subscriptions.length; i++) {
+            if (Objects.isNull(subscriptions[i])){
+                subscriptions[i] = abonement;
+                return;
             }
         }
+        System.out.println(Information.IS_CROWDED);
+    }
+
+
+    private boolean checkVisitTime(Subscription subscription){
+        if (subscription.getCurrentDate() < subscription.getStartTime()){
+            System.out.println("Вам придется немного подождать");
+            return true;
+        }else if (subscription.getCurrentDate() > subscription.getEndTime()){
+            System.out.println("Вы опоздали, приходите завтра");
+            return true;
+        }
+        return false;
     }
 
     boolean subscribe(Subscription subscription) {
